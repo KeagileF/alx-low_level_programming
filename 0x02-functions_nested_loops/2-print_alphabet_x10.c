ec37248f43edb5f5d14d66cfdd05378d4342aa25
KeagileF <keagilefainda@yahoo.com>
2023-02-21 16:49:15 -0800

    print alphabets x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,27 +1,21 @@
-i#include "main.h"
-
+#include "main.h"
 
 /**
- * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
- * followed by a new line
+ * print_alphabet_x10 - prints 10 times the alphabets
+ *
+ * Return: always 0
  */
+
 void print_alphabet_x10(void)
 {
-char ch;
-int i;
+	char j;
+	int i;
 
-
-i = 0;
-
-
-while (i < 10)
-{
-ch = 'a';
-while (ch <= 'z')
-{
-_putchar(ch);
-ch++;
-}
-_putchar('\n');
-i++;
+	for (i = 1 ; i <= 10 ; i++)
+	{
+		for (j = 'a' ; j <= 'z' ; j++)
+			_putchar(j);
+		_putchar('\n');
+			i++;
+	}
 }
